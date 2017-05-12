d0e2f17cc5ab3e61bc69b777a7dc1c4652b3233f
Siddharth Mittal <smittal6@gmail.com>
2017-05-12 23:54:03 +0530

    Begin with Overlap Problem

--- initial/EMimplement.py
+++ initial/EMimplement.py
@@ -22,15 +22,15 @@ def covmatrix(var,covar):
     return cov
 def initialize():
     means=np.random.rand(kgaussian)
-    print means.shape
+    # print means.shape
     var=np.random.rand(kgaussian)
-    print var.shape
+    # print var.shape
     covar=np.random.rand(kgaussian)
     cov=covmatrix(var,covar)
     mixingprob=np.random.random_sample(3)
     summix=np.sum(mixingprob)
     mixingprob=np.divide(mixingprob,summix)
-    print mixingprob
+    # print mixingprob
     return means,var,cov,mixingprob
 def evalloglike(data,means,var,cov,mixingprob):
     """ Computes the log likelihood
@@ -55,6 +55,7 @@ def posterior(n,k,data,means,var,mixingprob):
     """Evaluate the responsibilities
     """
     #When this function is to be called, make sure that n and k are 0 indexed
+    # print "Printing k value in posterior function: ",k
     numerator=mixingprob[k]*scipy.stats.norm(means[k],var[k]).cdf(data[n])
     denom=0
     for i in range(0,kgaussian):
@@ -62,37 +63,44 @@ def posterior(n,k,data,means,var,mixingprob):
     value=numerator/denom
     # print value
     return value
-def Nk(k,data,means,var,mixingprob):
+def calcposterior(k,data,means,var,mixingprob):
+    gammank_vector=np.zeros((k,datapoints))
+    for j in range(0,k):
+        for i in range(0,datapoints):
+            gammank_vector[j,i]=posterior(i,j,data,means,var,mixingprob)
+    return gammank_vector
+def Nk(k,data,means,var,mixingprob,gammavector):
     """Calculates N_k required for updates"""
-    nk=0
-    for i in range(0,datapoints):
-        nk=nk+posterior(i,k,data,means,var,mixingprob)
-    return nk
-def maximizestep(k,data,means,var,mixingprob):
+    return np.sum(gammavector[k,:])
+def maximizestep(k,data,means,var,mixingprob,gammank_vector):
     """We'll re-estimate the parameters, using current posterior probabilties.
     Maximizes only the kth parameter. Make sure k and n are 0 indexed
     We have k Guassian probabilites.
     """
-    nk=Nk(k,data,means,var,mixingprob)
-    gammank_vector=np.zeros((datapoints))
-    for i in range(0,datapoints):
-        gammank_vector[i]=posterior(i,k,data,means,var,mixingprob)
-    means[k]=np.dot(gammank_vector,data)/nk
+    nk=Nk(k,data,means,var,mixingprob,gammank_vector)
+    print "------"
+    print "Printing means vector before updating "
+    print means
+    means[k]=np.dot(gammank_vector[k,:],data)/nk
+    print "Printing means vector after updating ",k,"Gaussian"
+    print means
+    print "------"
     numerator=0
     for i in range(0,datapoints):
-        numerator=numerator+posterior(i,k,data,means,var,mixingprob)*np.square(data[i]-means[k])
+        numerator=numerator+gammank_vector[k,i]*np.square(data[i]-means[k])
     var[k]=numerator/nk
     mixingprob[k]=nk/datapoints
     return means,var,mixingprob
 def ExpectMax():
     means,var,cov,mixingprob=initialize()
+    means=[5,20,50]
+    var=[2,2,2]
+    mixingprob=[0.3333,0.3333,0.3333]
     print means
     print var
-    means=[5,20,50]
-    var=[2,5,6]
     data=np.random.normal(5,2,datapoints/3)
-    data2=np.random.normal(20,5,datapoints/3)
-    data3=np.random.normal(50,6,datapoints/3)
+    data2=np.random.normal(20,2,datapoints/3)
+    data3=np.random.normal(50,2,datapoints/3)
     plt.plot(data,color="r")
     plt.plot(data2,color="b")
     plt.plot(data3,color="g")
@@ -101,19 +109,22 @@ def ExpectMax():
     data=np.append(data,data2)
     data=np.append(data,data3)
     np.random.shuffle(data)
-    print data.shape
+    print "Shape check of data: ",data.shape
     likelihood=np.zeros((epoch))
     likelihood[0]=evalloglike(data,means,var,cov,mixingprob)
+    gammank_vector=calcposterior(kgaussian,data,means,var,mixingprob)
     for iterator in range(1,epoch):
         for i in range(0,kgaussian):
-            means,var,mixingprob=maximizestep(i,data,means,var,mixingprob)
+            means,var,mixingprob=maximizestep(i,data,means,var,mixingprob,gammank_vector)
         #Done updating[Maximizing for all the K guassian parameters]
         likelihood[iterator]=evalloglike(data,means,var,cov,mixingprob)
+        gammank_vector=calcposterior(kgaussian,data,means,var,mixingprob)
     print "The obtained means,var and mixing prob"
     print means
     print var
     print mixingprob
-    plt.plot(abs(likelihood))
+    ##Plotting of loglikelihood vs Epoch
+    plt.plot(likelihood)
     plt.title("Likelihood vs Epoch")
     plt.xlabel("Epochs")
     plt.ylabel("Log Likelihood value")
