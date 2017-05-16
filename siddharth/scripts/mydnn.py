f95be22b3ad201b82251f42af69dea6070b41ec2
Siddharth Mittal <smittal6@gmail.com>
2017-05-16 11:07:32 +0530

    correct indexing error

--- siddharth/scripts/mydnn.py
+++ siddharth/scripts/mydnn.py
@@ -22,22 +22,31 @@ import os
 np.random.seed(1337)
 trainfile="/home/smittal/Desktop/coding/leap/siddharth/combhtk/alltest.htk"
 testfile="/home/smittal/Desktop/coding/leap/siddharth/combhtk/alltrain.htk"
-
 def load_data_train(trainfile):
     a=htk.open(trainfile)
     data=a.getall()
-    x_train=data[:,data.shape[1]-1]
+    x_train=data[:,0:data.shape[1]-1]
     y_train=data[:,-1]
     return x_train,y_train
-
 def load_data_test(testfile):
     a=htk.open(testfile)
     data=a.getall()
-    x_test=data[:,data.shape[1]-1]
+    x_test=data[:,0:data.shape[1]-1]
     y_test=data[:,-1]
     return x_test,y_test
+def seq(x_train,y_train,x_test,y_test):
+    #Defining the structure of the neural network
+    #Creating a Network, with 2 hidden layers.
+    model=Sequential()
+    model.add()
+    model.add()
+
+    #Compilation region: Define optimizer, cost function, and the metric?
+
+    #Fitting region:Get to fit the model, with training data
 
 
+#Non-function section
 x_train,y_train=load_data_train(trainfile)
 print "Loading training data complete"
 print "Shape test: ",x_train.shape," ",y_train.shape
